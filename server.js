254c6ae203a1a87ace902e095292d0e231d6dfd2
DavidK1999 <davidkalina404@gmail.com>
2020-03-22 18:07:21 -0600

    make it better

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ let path = require('path')
 const app = express()
 const port = process.env.PORT
 const passport = require('passport')
-// const cors = require('cors')
+const cors = require('cors')
 require('./db/db.js')
 
 const allowedOrigins = ['http://localhost:3000/']
 
-// app.use(cors({
-//     origin: function(origin, callback) {
-//         if(!origin) return callback(null, true)
+app.use(cors({
+    origin: function(origin, callback) {
+        if(!origin) return callback(null, true)
         
-//         if(allowedOrigins.indexOf(origin) !== -1) {
-//             let msg = 'Not allowed by cors'
-//             return callback(new Error(msg), false)
-//         }
+        if(allowedOrigins.indexOf(origin) == -1) {
+            let msg = 'Not allowed by cors'
+            return callback(new Error(msg), false)
+        }
 
-//         return callback(null, true)
-//     }
-// }))
+        return callback(null, true)
+    }
+}))
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
